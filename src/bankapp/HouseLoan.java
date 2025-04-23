b90b323076b0c236a2804c93908f72887bfa0e4c
Refik <c.refik@wustl.edu>
2025-04-23 09:34:44 -0500

    Balances should be saved across iterations. Caution not tested thoroughly

--- src/bankapp/HouseLoan.java
+++ src/bankapp/HouseLoan.java
@@ -70,4 +70,56 @@ public class HouseLoan {
 	public double getLeftOverLoan() {
 		return leftOverLoan;
 	}
+	
+	
+	
+	/**
+	 * Serializes the house loan data to a string
+	 * @return string representation of loan data
+	 */
+	public String toDataString() {
+	    return String.join(":",
+	        String.valueOf(homePrice),
+	        String.valueOf(downPayment),
+	        String.valueOf(interestRate),
+	        String.valueOf(principalLoan),
+	        String.valueOf(leftOverLoan)
+	    );
+	}
+
+	/**
+	 * Deserializes house loan data from string
+	 * @param data string containing loan data
+	 */
+	public void fromDataString(String data) {
+	    String[] parts = data.split(":");
+	    if (parts.length == 5) {
+	        this.homePrice = Double.parseDouble(parts[0]);
+	        this.downPayment = Double.parseDouble(parts[1]);
+	        this.interestRate = Double.parseDouble(parts[2]);
+	        this.principalLoan = Double.parseDouble(parts[3]);
+	        this.leftOverLoan = Double.parseDouble(parts[4]);
+	    }
+	}
+
+	// Add setters for all fields that need to be modified
+	public void setHomePrice(double homePrice) {
+	    this.homePrice = homePrice;
+	}
+
+	public void setDownPayment(double downPayment) {
+	    this.downPayment = downPayment;
+	}
+
+	public void setInterestRate(double interestRate) {
+	    this.interestRate = interestRate;
+	}
+
+	public void setPrincipalLoan(double principalLoan) {
+	    this.principalLoan = principalLoan;
+	}
+
+	public void setLeftOverLoan(double leftOverLoan) {
+	    this.leftOverLoan = leftOverLoan;
+	}
 }
